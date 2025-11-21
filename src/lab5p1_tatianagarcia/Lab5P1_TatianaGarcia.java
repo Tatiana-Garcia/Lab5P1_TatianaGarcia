b642548f7b72198eb8769f10080c4c6a9a0b15b8
Tatiana-Garcia <tzgarcia_16@unitec.edu>
2025-11-21 13:53:20 -0600

    Comentario

---
+++ src/lab5p1_tatianagarcia/Lab5P1_TatianaGarcia.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab5p1_tatianagarcia;
+
+/**
+ *
+ * @author tatig
+ */
+public class Lab5P1_TatianaGarcia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
